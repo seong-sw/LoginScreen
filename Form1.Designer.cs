a8ffe319593ebd9685812f98f97058285f2be0ca
seong-sw <seong3533@suwon.ac.kr>
2026-04-02 11:47:33 +0900

    과제1 코딩과 테스트 완료
    Label과 TextBox, Button을 배치함
    txtID에 admin, txtPW에 superman을 입력하고 btnLogin을 누르면 로그인 성공 메시지 박스 출력
    아이디 혹은 비밀번호가 틀릴 경우 로그인 실패 메시지 박스 출력

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblAppName = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("Pretendard JP Variable SemiBold", 36F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            lblAppName.Location = new Point(216, 43);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(214, 86);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            // 
+            // txtID
+            // 
+            txtID.BorderStyle = BorderStyle.FixedSingle;
+            txtID.Font = new Font("Pretendard JP Variable", 24F);
+            txtID.Location = new Point(44, 183);
+            txtID.Name = "txtID";
+            txtID.PlaceholderText = "ID";
+            txtID.Size = new Size(572, 65);
+            txtID.TabIndex = 1;
+            // 
+            // txtPW
+            // 
+            txtPW.BorderStyle = BorderStyle.FixedSingle;
+            txtPW.Font = new Font("Pretendard JP Variable", 24F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            txtPW.Location = new Point(44, 297);
+            txtPW.Name = "txtPW";
+            txtPW.PlaceholderText = "Password";
+            txtPW.Size = new Size(572, 65);
+            txtPW.TabIndex = 2;
+            txtPW.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Pretendard JP Variable", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            btnLogin.Location = new Point(240, 415);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(162, 63);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(656, 526);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppName);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAppName;
+        private TextBox txtID;
+        private TextBox txtPW;
+        private Button btnLogin;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,28 @@ namespace LoginScreen
 {
     public partial class Form1 : Form
     {
+        string id = "admin";
+        string pw = "superman";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // 입력된 ID와 PW 값 가져옴
+            string inputID = txtID.Text;
+            string inputPW = txtPW.Text;
+
+            if (inputID == id && inputPW == pw)
+            {
+                MessageBox.Show("로그인 성공.", "로그인", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패.", "로그인", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
